cf65b61aba8a4fe807800a746a5e9220dbaf8a76
Rajiv <lucky81205@gmail.com>
2025-01-25 23:33:16 +0530

    new portfolio

---
+++ My-Portfolio/src/sections/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo-name">Rajiv</div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#project">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
