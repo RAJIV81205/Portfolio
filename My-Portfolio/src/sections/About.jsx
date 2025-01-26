f47fbb532dac6158d94a28e8e578f096fab5d985
Rajiv <lucky81205@gmail.com>
2025-01-26 16:36:17 +0530

    .

--- My-Portfolio/src/sections/About.jsx
+++ My-Portfolio/src/sections/About.jsx
@@ -3,11 +3,12 @@ function About() {
     <div className="about">
       <div className="div1"> </div>
       <div className="div2"> </div>
-      <div className="div3"> </div>
+      <div className="div3">
+      </div>
       <div className="div4"> </div>
       <div className="div5"> </div>
     </div>
   );
 }
 
-export default About
+export default About;
