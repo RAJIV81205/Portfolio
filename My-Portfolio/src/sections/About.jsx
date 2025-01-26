03e54df4afde7eed745d0fc005208b99c8406724
Rajiv <lucky81205@gmail.com>
2025-01-26 22:24:56 +0530

    .

--- My-Portfolio/src/sections/About.jsx
+++ My-Portfolio/src/sections/About.jsx
@@ -1,10 +1,16 @@
 function About() {
   return (
     <div className="about">
-      <div className="div1"> </div>
-      <div className="div2"> </div>
-      <div className="div3">
+      <div className="div1">
+        <img src="assets/grid1.png" alt="" />
+        <h3>Hi, I’m Adrian Hajdin</h3>
+        <p>
+          With 12 years of experience, I have honed my skills in both frontend
+          and backend dev, creating dynamic and responsive websites.
+        </p>
       </div>
+      <div className="div2"> </div>
+      <div className="div3"> </div>
       <div className="div4"> </div>
       <div className="div5"> </div>
     </div>
