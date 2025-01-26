19a2aa0963b5ee02287d8451e7b5698c94b2a660
Rajiv <lucky81205@gmail.com>
2025-01-26 14:03:49 +0530

    BUILDING ABOUT SEC

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/hero.jsx";
+import About from "./sections/About.jsx";
+import Footer from "./sections/Footer.jsx";
 
 function App(){
   return(
     <>
     <Header />
     <Hero />
+    <About />
+    <Footer />
 
     </>
   )
---
+++ My-Portfolio/src/sections/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div className="about">
+      <div className="div1"> </div>
+      <div className="div2"> </div>
+      <div className="div3"> </div>
+      <div className="div4"> </div>
+      <div className="div5"> </div>
+    </div>
+  );
+}
+
+export default About
