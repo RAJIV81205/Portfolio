2a04fe4d064c19520666d42a44fef46f68052ef4
Rajiv <lucky81205@gmail.com>
2025-01-26 23:00:46 +0530

    .

--- My-Portfolio/src/sections/About.jsx
+++ My-Portfolio/src/sections/About.jsx
@@ -3,13 +3,14 @@ function About() {
     <div className="about">
       <div className="div1">
         <img src="assets/grid1.png" alt="" />
-        <h3>Hi, I’m Adrian Hajdin</h3>
+        <h3>Hi, I’m Rajiv Dubey</h3>
         <p>
           With 12 years of experience, I have honed my skills in both frontend
           and backend dev, creating dynamic and responsive websites.
         </p>
       </div>
-      <div className="div2"> </div>
+      <div className="div2">
+        <img src="assets/grid2.png" alt="" /> </div>
       <div className="div3"> </div>
       <div className="div4"> </div>
       <div className="div5"> </div>
