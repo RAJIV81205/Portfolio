0e4cfdebe695d447e96af901c153f8d76775b6c1
Rajiv <lucky81205@gmail.com>
2025-01-28 13:19:21 +0530

    added sparks

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,18 +1,27 @@
 import Header from "./sections/Header.jsx";
-import Hero from "./sections/hero.jsx";
+import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Footer from "./sections/Footer.jsx";
+import ClickSpark from "./components/ClickSpark.jsx";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <Header />
-    <Hero />
-    <About />
-    <Footer />
+      
+      <Header />
+      <Hero />
+      <About />
+      <Footer />
 
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
