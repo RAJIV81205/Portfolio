151cf792cbfc5c877c14f021ca5d6c4580dd501d
Rajiv <lucky81205@gmail.com>
2025-01-28 13:26:02 +0530

    removing spark

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Footer from "./sections/Footer.jsx";
-import ClickSpark from "./components/ClickSpark.jsx";
+
 
 function App() {
   return (
@@ -13,13 +13,7 @@ function App() {
       <About />
       <Footer />
 
-      <ClickSpark
-        sparkColor="#fff"
-        sparkSize={10}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={400}
-      />
+      
     </>
   );
 }
