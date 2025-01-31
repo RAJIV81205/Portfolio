b8633c676010de413a765eef012ace1953fd9c18
Rajiv <lucky81205@gmail.com>
2025-01-31 14:15:51 +0530

    .

--- My-Portfolio/src/components/constant.js
+++ My-Portfolio/src/components/constant.js
@@ -1,125 +1,97 @@
-export const project = [{
-    title: 'BookHive - Online Book Store',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
-    subdesc:
-        'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://bookhive-99.firebaseapp.com/',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/bookhive.png',
-    logoStyle: {
-        backgroundColor: '#2A1816',
-        border: '1px solid #52290a',
-        boxShadow: '0px 0px 60px 0px #fcae05',
+export const project = [
+    {
+        title: 'BookHive - Online Book Store',
+        desc: 'BookHive is an online platform that brings book lovers together, offering a seamless experience for browsing, purchasing, and exploring a vast collection of books across various genres.',
+        subdesc:
+            'Built using modern web technologies like HTML, CSS, JavaScript, Node.js, and MongoDB, BookHive ensures a smooth and interactive user experience for book enthusiasts.',
+        href: 'https://bookhive-99.web.app/',
+        texture: '/textures/project/project1.mp4',
+        logo: '/assets/bookhive.png',
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '1px solid #52290a',
+            boxShadow: '0px 0px 60px 0px #fcae05',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'HTML',
+                path: '/assets/html.png',
+            },
+            {
+                id: 2,
+                name: 'CSS',
+                path: 'assets/css.png',
+            },
+            {
+                id: 3,
+                name: 'JavaScript',
+                path: '/assets/js.png',
+            },
+            {
+                id: 4,
+                name: 'Node.js',
+                path: '/assets/node.png',
+            },
+            {
+                id: 5,
+                name: 'MongoDB',
+                path: '/assets/mongo.svg',
+            },
+        ],
     },
-    spotlight: '/assets/spotlight1.png',
-    tags: [
-        {
-            id: 1,
-            name: 'HTML',
-            path: '/assets/html.png',
+    {
+        title: 'ChatFly - Online Chat Application',
+        desc: 'ChatFly is a real-time messaging platform that allows users to connect and communicate seamlessly. With a user-friendly interface and secure messaging, it offers an engaging chat experience.',
+        subdesc:
+            'Built using modern web technologies like HTML, CSS, JavaScript, Node.js, MongoDB, and Socket IO, ChatFly ensures fast, reliable, and secure real-time communication for users.',
+        href: 'https://chatfly.onrender.com/',
+        texture: '/textures/project/project1.mp4',
+        logo: '/assets/chatfly.png', 
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '1px solid #52290a',
+            boxShadow: '0px 0px 60px 0px #fcae05',
         },
-        {
-            id: 2,
-            name: 'CSS',
-            path: 'assets/css.png',
-        },
-        {
-            id: 3,
-            name: 'Javascript',
-            path: '/assets/js.png',
-        },
-        {
-            id: 4,
-            name: 'Node.js',
-            path: '/assets/node.png',
-        },
-        {
-            id: 5,
-            name: 'Mongo DB',
-            path: '/assets/mongo.svg',
-        },
-    ],
-}, {
-    title: '1 - Online Book Store',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
-    subdesc:
-        'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://bookhive-99.firebaseapp.com/',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/bookhive.png',
-    logoStyle: {
-        backgroundColor: '#2A1816',
-        border: '1px solid #52290a',
-        boxShadow: '0px 0px 60px 0px #fcae05',
-    },
-    spotlight: '/assets/spotlight1.png',
-    tags: [
-        {
-            id: 1,
-            name: 'HTML',
-            path: '/assets/html.png',
-        },
-        {
-            id: 2,
-            name: 'CSS',
-            path: 'assets/css.png',
-        },
-        {
-            id: 3,
-            name: 'Javascript',
-            path: '/assets/js.png',
-        },
-        {
-            id: 4,
-            name: 'Node.js',
-            path: '/assets/node.png',
-        },
-        {
-            id: 5,
-            name: 'Mongo DB',
-            path: '/assets/mongo.svg',
-        },
-    ],
-},{title: '2 - Online Book Store',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
-    subdesc:
-        'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://bookhive-99.firebaseapp.com/',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/bookhive.png',
-    logoStyle: {
-        backgroundColor: '#2A1816',
-        border: '1px solid #52290a',
-        boxShadow: '0px 0px 60px 0px #fcae05',
-    },
-    spotlight: '/assets/spotlight1.png',
-    tags: [
-        {
-            id: 1,
-            name: 'HTML',
-            path: '/assets/html.png',
-        },
-        {
-            id: 2,
-            name: 'CSS',
-            path: 'assets/css.png',
-        },
-        {
-            id: 3,
-            name: 'Javascript',
-            path: '/assets/js.png',
-        },
-        {
-            id: 4,
-            name: 'Node.js',
-            path: '/assets/node.png',
-        },
-        {
-            id: 5,
-            name: 'Mongo DB',
-            path: '/assets/mongo.svg',
-        },
-    ],}
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'HTML',
+                path: '/assets/html.png',
+            },
+            {
+                id: 2,
+                name: 'CSS',
+                path: '/assets/css.png',
+            },
+            {
+                id: 3,
+                name: 'JavaScript',
+                path: '/assets/js.png',
+            },
+            {
+                id: 4,
+                name: 'Node.js',
+                path: '/assets/node.png',
+            },
+            {
+                id: 5,
+                name: 'MongoDB',
+                path: '/assets/mongo.svg',
+            },
+            {
+                id: 6,
+                name: 'Socket.io',
+                path: '/assets/socket.png', 
+            },
+        ],
+    }
+
+
+
+
 ];
 
 
