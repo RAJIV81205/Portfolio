aca6fddc30ee86c65d84b16517218ed4363c4dab
Rajiv <lucky81205@gmail.com>
2025-02-02 13:37:17 +0530

    .

--- My-Portfolio/src/components/constant.js
+++ My-Portfolio/src/components/constant.js
@@ -87,6 +87,42 @@ export const project = [
                 path: '/assets/socket.png', 
             },
         ],
+    },{
+        title: 'Easy-Rail - Rail Solution',
+        desc: 'ChatFly is a real-time messaging platform that allows users to connect and communicate seamlessly. With a user-friendly interface and secure messaging, it offers an engaging chat experience.',
+        subdesc:
+            'Built using modern web technologies like HTML, CSS, JavaScript, Node.js, MongoDB, and Socket IO, ChatFly ensures fast, reliable, and secure real-time communication for users.',
+        href: 'https://easy-rail.onrender.com/',
+        texture: '/textures/project/project1.mp4',
+        logo: '/assets/easy-rail.png', 
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '1px solid #770e0c',
+            boxShadow: '0px 0px 60px 0px #fcae05',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'HTML',
+                path: '/assets/html.png',
+            },
+            {
+                id: 2,
+                name: 'CSS',
+                path: '/assets/css.png',
+            },
+            {
+                id: 3,
+                name: 'JavaScript',
+                path: '/assets/js.png',
+            },
+            {
+                id: 4,
+                name: 'Node.js',
+                path: '/assets/node.png',
+            },
+        ],
     }
 
 
