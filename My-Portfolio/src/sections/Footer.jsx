6c4592ce4dc059f7ea825d49655aacc68cf4d41e
Rajiv <lucky81205@gmail.com>
2025-02-05 23:12:18 +0530

    .

--- My-Portfolio/src/sections/Footer.jsx
+++ My-Portfolio/src/sections/Footer.jsx
@@ -1,7 +1,22 @@
-function Footer(){
-    return(
-        <footer></footer>
-    )
+import RotatingText from "../components/RotatingText";
+
+function Footer() {
+  return (
+    <footer>
+      <RotatingText
+        texts={["React", "Bits", "Is", "Cool!"]}
+        mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+        staggerFrom={"last"}
+        initial={{ y: "100%" }}
+        animate={{ y: "0" }}
+        exit={{ y: "-120%" }}
+        staggerDuration={0.025}
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        transition={{ type: "spring", damping: 30, stiffness: 400 }}
+        rotationInterval={2000}
+      />
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
