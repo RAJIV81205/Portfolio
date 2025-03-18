1d18e72806403499e87b4a2cbd763547d5de43ad
Rajiv <lucky81205@gmail.com>
2025-03-18 23:01:30 +0530

    raktconnect

--- My-Portfolio/src/components/constant.js
+++ My-Portfolio/src/components/constant.js
@@ -42,13 +42,56 @@ export const project = [
         ],
     },
     {
+        title: 'RaktConnect',
+        desc: 'RaktConnect is a platform that connects blood donors with those in need of blood. It allows users to find blood donors, request blood, and donate blood.',
+        subdesc:
+            'Built using modern web technologies like React, Tailwind CSS, and Node.js, this platform highlights my expertise in frontend and backend development, featuring responsive design and smooth animations.',
+        href: 'https://raktconnect.onrender.com/',
+        texture: '/assets/raktconnect.mp4',
+        logo: '/assets/raktconnect.png',
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '1px solid#d62c2c',
+            boxShadow: '0px 0px 60px 0px #fcae05',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'Tailwind CSS',
+                path: '/assets/tailwind.png',
+            },
+            {
+                id: 3,
+                name: 'Node.js',
+                path: '/assets/node.png',
+            },
+            {
+                id: 4,
+                name: 'MongoDB',
+                path: '/assets/mongo.svg',
+            },
+            {
+                id: 5,
+                name: 'Python',
+                path: '/assets/python.svg',
+            },
+            
+        ],
+    },
+    {
         title: 'ChatFly - Online Chat Application',
         desc: 'ChatFly is a real-time messaging platform that allows users to connect and communicate seamlessly. With a user-friendly interface and secure messaging, it offers an engaging chat experience.',
         subdesc:
             'Built using modern web technologies like HTML, CSS, JavaScript, Node.js, MongoDB, and Socket IO, ChatFly ensures fast, reliable, and secure real-time communication for users.',
         href: 'https://chatfly.onrender.com/',
         texture: '/textures/project/project1.mp4',
-        logo: '/assets/chatfly.png', 
+        logo: '/assets/chatfly.png',
         logoStyle: {
             backgroundColor: '#2A1816',
             border: '1px solid #52290a',
@@ -84,17 +127,17 @@ export const project = [
             {
                 id: 6,
                 name: 'Socket.io',
-                path: '/assets/socket.png', 
+                path: '/assets/socket.png',
             },
         ],
-    },{
+    }, {
         title: 'Easy-Rail - Rail Solution',
         desc: 'Easy-Rail is a comprehensive railway information platform that helps users check train schedules, PNR status, live train status, and upcoming trains with ease.',
         subdesc:
             'Built using modern web technologies like HTML, CSS, JavaScript, and Node.js, Easy-Rail ensures a seamless experience for railway travelers by providing accurate and real-time train information.',
         href: 'https://easy-rail.onrender.com/',
         texture: '/assets/easyrail.mp4',
-        logo: '/assets/easy-rail.png', 
+        logo: '/assets/easy-rail.png',
         logoStyle: {
             backgroundColor: '#2A1816',
             border: '1px solid #770e0c',
@@ -123,14 +166,14 @@ export const project = [
                 path: '/assets/node.png',
             },
         ],
-    },{
+    }, {
         title: 'Portfolio',
         desc: 'A personal portfolio website showcasing my projects, skills, and experience in web development. It serves as a digital resume and an interactive platform to explore my work.',
         subdesc:
             'Built using modern web technologies like React, CSS, and JavaScript, this portfolio highlights my expertise in frontend and backend development, featuring responsive design and smooth animations.',
         href: 'https://rajiv-dubey.onrender.com/',
-        texture: '/assets/easyrail.mp4', 
-        logo: '/assets/portfolio.png', 
+        texture: '/assets/easyrail.mp4',
+        logo: '/assets/portfolio.png',
         logoStyle: {
             backgroundColor: '#2A1816',
             border: '1px solid #6d0676',
@@ -155,8 +198,8 @@ export const project = [
             },
         ],
     }
-    
-    
+
+
 
 
 
